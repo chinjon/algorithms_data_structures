a76ae920864baa4a1f3948a2b953ddbcea117bcc
Jonathan Chin <jonathankchin@gmail.com>
2021-08-21 10:55:44 -0400

    feat: init commit

---
+++ hackerrank/diagnonal-difference/diagonal-difference.js
@@ -0,0 +1,19 @@
+/**
+ * Example Input:
+ * 
+ * 1 2 3
+ * 4 5 6
+ * 9 8 9 
+ * 
+ * Expected Output:
+ * 
+ * 1 + 5 + 9 = 15
+ * 3 + 5 + 9 = 13
+ * | 15 - 17| = 2 (absolute difference)
+ */
+
+
+function diagonalDifference(arr) {
+  // Write your code here
+
+}
